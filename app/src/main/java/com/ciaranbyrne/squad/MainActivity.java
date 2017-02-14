3d82f272776a2e06abc000a5029ed57ab7768daf
Ciaran <ciaran426@yahoo.co.uk>
2017-02-14 19:59:59 +0000

    looking at friendlychat auth

--- app/src/main/java/com/ciaranbyrne/squad/MainActivity.java
+++ app/src/main/java/com/ciaranbyrne/squad/MainActivity.java
@@ -1,11 +1,13 @@
 package com.ciaranbyrne.squad;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.email.SignInActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -13,20 +15,43 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     //CONSTANTS
+    public static final String ANONYMOUS = "anonymous";
+
     public static final int RC_SIGN_IN = 1;
 
     //Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+    private String mUsername;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Initialize Firebase components
-        mFirebaseAuth = FirebaseAuth.getInstance();
+        mUsername = ANONYMOUS;
 
+        //Initialize Firebase components
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+           /* if (mFirebaseUser.getPhotoUrl() != null) {
+                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            }
+            */
+        }
         // Firebase Authentication state listener
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -55,6 +80,23 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
+    //TODO
+    //sign out button STEP 6 - need to build button xml, getting error on mGoogleApiclient - can try alternate one in FriendlyChat
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                mFirebaseAuth.signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                mUsername = ANONYMOUS;
+                startActivity(new Intent(this, SignInActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    */
 
     protected void onPause(){
         super.onPause();
