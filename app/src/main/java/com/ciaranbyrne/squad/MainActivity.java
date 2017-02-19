826e9f3e88830febf3eed4f10a86d13ddbb4fe7a
Ciaran <ciaran426@yahoo.co.uk>
2017-02-19 23:08:39 +0000

    authentication working

--- app/src/main/java/com/ciaranbyrne/squad/MainActivity.java
+++ app/src/main/java/com/ciaranbyrne/squad/MainActivity.java
@@ -1,13 +1,12 @@
 package com.ciaranbyrne.squad;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ui.email.SignInActivity;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,24 +14,29 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     //CONSTANTS
+    public static final int RC_SIGN_IN = 1;
+    private static final String TAG = "MainActivity";
     public static final String ANONYMOUS = "anonymous";
 
-    public static final int RC_SIGN_IN = 1;
 
     //Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
-
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
 
     private String mUsername;
+    private GoogleApiClient mGoogleApiClient;
+    private SharedPreferences mSharedPreferences;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         mUsername = ANONYMOUS;
 
@@ -52,34 +56,55 @@ public class MainActivity extends AppCompatActivity {
             }
             */
         }
-        // Firebase Authentication state listener
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                //check if this user is logged in
-                if (user != null){
-                    //user is signed in
-                    Toast.makeText(MainActivity.this, "You;re signed in",Toast.LENGTH_SHORT).show();
 
-                }else {
-                    //user is signed out - sign in flow needed
-                    startActivityForResult(
-                            AuthUI.getInstance()
-                                    .createSignInIntentBuilder()
-                                    // saves users credentials to try log them in - set to false
-                                    .setIsSmartLockEnabled(false)
-                                    .setProviders(
-                                            AuthUI.EMAIL_PROVIDER,
-                                            AuthUI.GOOGLE_PROVIDER
-                                    )
-                                    .build(),
-                            RC_SIGN_IN);
-                }
-
-            }
-        };
     }
+
+        // Firebase Authentication state listener
+
+
+/*
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    */
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in.
+        // TODO: Add code to check if user is signed in.
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+   /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    */
+
+
     //TODO
     //sign out button STEP 6 - need to build button xml, getting error on mGoogleApiclient - can try alternate one in FrienlyCha
     /*
@@ -97,18 +122,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     */
-
-    protected void onPause(){
-        super.onPause();
-        //remove Authentication state listener
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
-    }
-
-    protected void onResuem(){
-        super.onResume();
-        //attach Firebase Authentication state listener
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
-    }
-
 
 }
